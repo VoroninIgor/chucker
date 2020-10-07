9c7689bba642d712fe83ad1150c50c7f5fb6c099
angai <angainor17@gmail.com>
2020-10-07 21:07:19 +0300

    list of words

--- library/src/main/java/com/chuckerteam/chucker/internal/support/TransactionCurlCommandSharable.kt
+++ library/src/main/java/com/chuckerteam/chucker/internal/support/TransactionCurlCommandSharable.kt
@@ -2,6 +2,8 @@ package com.chuckerteam.chucker.internal.support
 
 import android.content.Context
 import com.chuckerteam.chucker.internal.data.entity.Transaction
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 import okio.Buffer
 import okio.Source
 
@@ -13,11 +15,11 @@ internal class TransactionCurlCommandSharable(
 
         val headers = transaction.getParsedRequestHeaders()
         if (headers?.isNotEmpty() == true) {
-            writeUtf8(" -H")
+            writeUtf8(" -H ")
 
             writeUtf8("\"")
             headers.forEachIndexed { index, header ->
-                writeUtf8("${header.name} :${header.value}")
+                writeUtf8("${header.name}: ${header.value}")
                 if (index > 0) {
                     writeUtf8(", ")
                 }
@@ -25,14 +27,39 @@ internal class TransactionCurlCommandSharable(
             writeUtf8("\"")
         }
 
-        val requestBody = transaction.requestBody ?: "".replace("\n","")
+        var requestBody = transaction.requestBody ?: "".replace("\n", "")
         if (requestBody.isNotEmpty()) {
             writeUtf8(" -d")
-            writeUtf8(" \"${requestBody.replace("\n", "\\n")}\"")
+            writeUtf8(" \"{")
+
+            val pattern: Pattern = Pattern.compile("[a-zA-Z]+")
+            val matcher: Matcher = pattern.matcher(requestBody)
+            var list = ArrayList<String>()
+            while (matcher.find()) {
+                list.add(matcher.group())
+            }
+            list = removeDuplicates(list)
+            list.forEach { word->
+                requestBody = requestBody.replace(word, "\"$requestBody\"")
+            }
+
+            writeUtf8(requestBody)
+            writeUtf8("}\"")
         }
 
         writeUtf8(" " + transaction.urlFormatted)
         writeUtf8(" " + transaction.method)
+    }
 
+    private fun <T> removeDuplicates(list: ArrayList<T>): ArrayList<T> {
+        val newList = ArrayList<T>()
+
+        for (element in list) {
+            if (!newList.contains(element)) {
+                newList.add(element)
+            }
+        }
+
+        return newList
     }
 }
